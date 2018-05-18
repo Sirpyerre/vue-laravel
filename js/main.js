fb36be92e75be15366e4d491ef430069fbcceed9
Peter <pedro.rojas@gmail.com>
2018-05-18 18:54:53 -0500

    componentes en componentes

--- js/main.js
+++ js/main.js
@@ -35,6 +35,33 @@ Vue.component('modal',{
 // 	}
 // });
 
+Vue.component('task-list', {
+	template : `
+		<div>
+			<task v-for="task in tasks">{{ task.task }}</task>
+		</div>
+		`,
+	data() {
+		return {
+			tasks: [
+				{
+					task: 'Go to the store', complete: true
+				},
+				{
+					task: 'Go to the email', complete: false
+				},
+				{
+					task: 'Go to the farm', complete: true
+				},
+				{
+					task: 'Go to the work', complete: false
+				}
+			]
+		}
+	}
+});
+
+
 Vue.component('task', {
 	'template' : '<li><slot></slot></li>'
 });
